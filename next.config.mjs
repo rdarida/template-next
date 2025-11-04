98a9c012b13c238d33e9119ae6018845eb5ef499
Róbert Darida <rdarida@users.noreply.github.com>
2025-11-04 23:22:29 +0100

    feat: set VERSION constant in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
+import { resolve } from 'node:path';
+import { readFileSync } from 'node:fs';
+
+const packageJsonPath = resolve(process.cwd(), 'package.json');
+const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_VERSION: packageJson.version
+  },
   reactStrictMode: true,
   redirects: async () => [
     { source: '/home', destination: '/', permanent: true }
--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
-import { version } from '../package.json';
-
 export const HOST = process.env.NEXT_PUBLIC_HOST ?? '';
 
-export const VERSION = version ?? '0.0.0';
+export const VERSION = process.env.NEXT_PUBLIC_VERSION ?? '0.0.0';
 
 export const NAME = process.env.NEXT_PUBLIC_NAME ?? '';
 
